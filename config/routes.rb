211925ea3f45f8c5ece94543dbf7c84ffc055290
Loïc PIREZ <loic@pirez.fr>
2023-05-27 18:45:04 +0200

    Another CRUD controller with Turbo Rails (adding dates to quotes)

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: %i[index show]
+  end
 end
